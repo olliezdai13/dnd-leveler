707946ef68e9cd2cb1f47d873f224acd6933ccf2
Oliver <olliezdai@gmail.com>
2021-03-29 23:20:36 -0400

    added database service

---
+++ database/db.js
@@ -0,0 +1,17 @@
+const mysql2 = require('mysql2/promise');
+
+async function query(sql, params) {
+  const connection = await mysql2.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'ddcharactermanager'
+  });
+  const [results, ] = await connection.execute(sql, params);
+
+  return results;
+}
+
+module.exports = {
+  query
+}
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var db = require('../database/db');
 var router = express.Router();
 
 /* GET home page. */
