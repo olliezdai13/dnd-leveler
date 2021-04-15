c591ee067d76d646029f9bc7cfe8fdb76e2ef6ec
Oliver <olliezdai@gmail.com>
2021-04-15 15:43:19 -0400

    Home page, character info page, character edit page template

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,44 @@ var db = require('../database/db');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res) {
+  const rows = await db.query(`SELECT * FROM ddcharacter;`);
+  console.log(rows);
+  res.render('home', 
+  { 
+    "characterlist": rows,
+    title: 'D&D Character Leveling Tool' 
+  });
 });
 
-/* GET Hello World page. */
-router.get('/helloworld', function(req, res, next) {
-  res.render('helloworld', { title: 'Hello World!' });
+/* GET character page. */
+router.get('/character/:cid', async function(req, res) {
+  const cid = req.params.cid;
+  const rows = await db.query(`SELECT * FROM ddcharacter dd JOIN race rr ON dd.race_name = rr.race_name JOIN class cc ON dd.class_name = cc.class_name JOIN background bb ON dd.bg_name = bb.bg_name WHERE character_id = ?;`, [cid]);
+  console.log(rows[0]);
+  const statrows = await db.query(`CALL calc_character_stats(?)`, [cid]);
+  console.log(statrows[0][0]);
+  res.render('characterinfo', 
+  { 
+    "character": rows[0],
+    "stats": statrows[0][0],
+    title: rows[0].character_name 
+  });
+});
+
+/* GET character edit page. */
+router.get('/character/:cid/edit', async function(req, res) {
+  const cid = req.params.cid;
+  const rows = await db.query(`SELECT * FROM ddcharacter dd JOIN race rr ON dd.race_name = rr.race_name JOIN class cc ON dd.class_name = cc.class_name JOIN background bb ON dd.bg_name = bb.bg_name WHERE character_id = ?;`, [cid]);
+  console.log(rows[0]);
+  const statrows = await db.query(`CALL calc_character_stats(?)`, [cid]);
+  console.log(statrows[0][0]);
+  res.render('characteredit', 
+  { 
+    "character": rows[0],
+    "stats": statrows[0][0],
+    title: rows[0].character_name 
+  });
 });
 
 /* GET race list page */
